55e76b3ab7e835fa1dd877c22842ba13e278eb96
Christian Landgren <christian@landgren.nu>
2024-02-24 06:40:00 +0100

    typo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -61,7 +61,7 @@ export const Footer = () => {
           &copy; 2024{' '}
           <a
             target="_blank"
-            href="https://github.com/berget"
+            href="https://github.com/berget-cloud"
             className="text-primary transition-all border-primary hover:border-b-2"
           >
             Molnet i Berget AB
