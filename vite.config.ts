7dcc66203197e577e240bcb73ce9eb7b806c665c
Christian Landgren <christian@landgren.nu>
2024-02-28 20:46:02 +0100

    feat: split vendor chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import path from 'path'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
   plugins: [
     react(),
-
+    splitVendorChunkPlugin(),
     ViteImageOptimizer({
       png: {
         quality: 80,
